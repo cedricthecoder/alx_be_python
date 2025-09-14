6b9ed96de9c8df636fda151ecf58d7138d8b5010
cedricthecoder <nngaboced@gmail.com>
2025-09-14 20:33:30 +0200

    added the simple_interest.py file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#Declaring the main variables Interest, principal, rate, and time and respective values.
+
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
