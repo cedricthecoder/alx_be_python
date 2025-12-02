59d674b1eee8d788a8cbce50e028ec2960b69a34
cedricthecoder <nngaboced@gmail.com>
2025-12-02 22:31:38 +0200

    Created the basic arithmetic operations file, named it arithmetic operations, alongside main.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+def perform_operation(num1,num2,operation):
+    """num1 = None
+    num2 = None
+    store_number = None
+    operation = """
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            print("Cannot Divide by zero")
+        else: 
+            return num1 / num2
+    #else:
+        #print("Please Try again!")
+
+#perform_operation(num1,num2,operation)
