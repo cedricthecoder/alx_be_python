dcb197bec40719b391e891ff82bc5fe7f5a00cd3
cedricthecoder <nngaboced@gmail.com>
2025-12-03 18:36:48 +0200

    modified the functionalities to include the option to delete and append to the list properly.

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -7,26 +7,39 @@ def display_menu():
     print("4. Exit")
 
 def main():
-    shopping_list = []
+    global shopping_list
+    
     while True:
         display_menu()
         choice = input("Enter your choice: ")
 
         if choice == '1':
             # Prompt for adding an item
-            print("Enter item you want to adding:")
-            return shopping_list.append()
-            print(f"The new shopping list: {shopping_list}")
+            name = input("Enter the item to add:")
+            shopping_list.append(name)
+            print(f"The new shopping list is: {shopping_list}")
             pass
+        
         elif choice == '2':
             # Prompt for and remove an item
+            item_to_remove =input("Enter item to remove:")
+            
+            if item_to_remove in shopping_list:
+                shopping_list.remove(item_to_remove)
+                print(f"The new shopping list: {shopping_list}")
+            else:
+                print(f"The item {item_to_remove} is not found in the shopping list.")
             pass
+        
         elif choice == '3':
             # Display the shopping list
+            print(f"The list is: {shopping_list}")
             pass
+        
         elif choice == '4':
             print("Goodbye!")
             break
+        
         else:
             print("Invalid choice. Please try again.")
 
