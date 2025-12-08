7f222439cf31b8c061ea85fa26819d1cfa317375
cedricthecoder <nngaboced@gmail.com>
2025-12-09 00:38:52 +0200

    added the main class of book with attributes year, author and title.

---
+++ oop/book_class.py
@@ -0,0 +1,20 @@
+class Book:
+    """A class to represent a book."""
+
+    def __init__(self, title, author, year):
+        """Constructor to initialize a Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor to print a message when a Book instance is deleted."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """String representation for informal use."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Official string representation for recreating the Book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
